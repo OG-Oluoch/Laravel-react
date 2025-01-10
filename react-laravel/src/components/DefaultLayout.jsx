7dc7da5116cbcd603789813075c50cd4eebcc10c
OG-Oluoch <makopiyo45@gmail.com>
2025-01-10 12:06:18 +0300

    more UI

--- react-laravel/src/components/DefaultLayout.jsx
+++ react-laravel/src/components/DefaultLayout.jsx
@@ -1,10 +1,42 @@
-import {Outlet} from "react-router-dom";
+import {Link, Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function DefaultLayout(){
+  const {user,token}= useStateContext()
+
+  if(!token){
+
+    return <Navigate to="/login"/>
+  }
+
+  const onLogout=(ev)=>{
+    ev.preventDefault();
+  }
+
  return (
- <div>
-   Default
-<Outlet/>
+ <div id="defaultLayout">
+   <aside>
+     <Link to="/dashboard">Dashboard</Link>
+     <Link to="/users">Users</Link>
+   </aside>
+   <div className="content">
+     <header>
+       <div>
+         Header
+       </div>
+        <div>
+          {user.name}
+          <a href="#" className="btn-logout" onClick={onLogout}></a>
+        </div>
+     </header>
+
+     <main>
+       <Outlet/>
+     </main>
+
+
+   </div>
+
  </div>
  )
 }
--- react-laravel/src/views/Login.jsx
+++ react-laravel/src/views/Login.jsx
@@ -1,7 +1,24 @@
+import {Link} from "react-router-dom";
+
 export default function Login(){
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+  }
+
  return (
- <div>
-    Login
+ <div className="Login-signup-form animated fadeInDown">
+    <div className="form">
+      <form onSubmit={onSubmit}>
+        <h1>Log in to your account</h1>
+        <input placeholder="Email" name="email" type="email" />
+        <input placeholder="Password" name="password" type="password" />
+        <button className="btn btn-block">Login</button>
+        <p className="message">
+          Not Registered?  <Link to="/signup">Create an account</Link>
+        </p>
+      </form>
+    </div>
  </div>
  )
 }
